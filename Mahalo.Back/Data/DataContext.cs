9e73f8bc2f103baaab74158a9545aa09b55c1400
Edwin Arley Tangarife Tobón <eatangaife@gmail.com>
2024-09-22 22:12:54 -0500

    Prueba funcional de consulta a base de datos

---
+++ Mahalo.Back/Data/DataContext.cs
@@ -0,0 +1,33 @@
+﻿using Mahalo.Shared.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Mahalo.Back.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+
+
+        public DbSet<Country> Countries { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Country>().HasIndex(x => x.Name).IsUnique();
+            DisableCascadingDelete(modelBuilder);
+        }
+
+        private void DisableCascadingDelete(ModelBuilder modelBuilder)
+        {
+            var relationShips = modelBuilder.Model.GetEntityTypes().SelectMany(e => e.GetForeignKeys());
+            foreach (var relationShip in relationShips)
+            {
+                relationShip.DeleteBehavior = DeleteBehavior.Restrict;
+            }
+        }
+
+    }
+}
--- Mahalo.Back/Program.cs
+++ Mahalo.Back/Program.cs
@@ -1,3 +1,10 @@
+using Mahalo.Back.Data;
+using Mahalo.Back.Repositories.Implementation;
+using Mahalo.Back.Repositories.Interfaces;
+using Mahalo.Back.UnitsOfWork.Implementation;
+using Mahalo.Back.UnitsOfWork.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +13,9 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<DataContext>(x => x.UseSqlServer("name=LocalConnection"));
+builder.Services.AddScoped(typeof(IGenericUnitOfWork<>), typeof(GenericUnitOfWork<>));
+builder.Services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
 
 var app = builder.Build();
 
