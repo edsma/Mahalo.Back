8b24ea914e3827457cb1c1100437858d3c7a5a51
Catherine Delgado Paladines <katdpds@gmail.com>
2024-09-25 00:14:41 -0500

    add documentTypesController template

--- Mahalo.Back/Controllers/CountriesController.cs
+++ Mahalo.Back/Controllers/CountriesController.cs
@@ -2,14 +2,13 @@
 using Mahalo.Shared.Entities;
 using Microsoft.AspNetCore.Mvc;
 
-namespace Mahalo.Back.Controllers
+namespace Mahalo.Back.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class CountriesController : GenericController<Country>
 {
-    [ApiController]
-    [Route("api/[controller]")]
-    public class CountriesController : GenericController<Country>
+    public CountriesController(IGenericUnitOfWork<Country> unitOfWork) : base(unitOfWork)
     {
-        public CountriesController(IGenericUnitOfWork<Country> unitOfWork) : base(unitOfWork)
-        {
-        }
     }
 }
---
+++ Mahalo.Back/Controllers/DocumentTypesController.cs
@@ -0,0 +1,16 @@
+﻿using Mahalo.Back.Data;
+using Mahalo.Back.UnitsOfWork.Interfaces;
+using Mahalo.Shared.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Mahalo.Back.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class DocumentTypesController : GenericController<DocumentType>
+{
+    public DocumentTypesController(IGenericUnitOfWork<DocumentType> unitOfWork) : base(unitOfWork)
+    {
+            
+    }
+}
